b9c4a3d8f579e0c3472045c308b5c74f8f6853c6
hadil bourbouna <you@example.com>
2022-11-30 21:14:33 +0100

    question 4

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -4,18 +4,26 @@ import java.sql.SQLException;
 
 public class DBConnection {
 	   
-		String BDD = "nomBD";
+		String BDD = "e-book";
 		String url = "jdbc:mysql://localhost:3306/" + BDD;
 		String user = "root";
 		String passwd = "";
 	    private Connection conn;
+	    private static DBConnection instance;
 
 	   
 	    public DBConnection() throws SQLException {
-			conn=DriverManager.getConnection(url, user,passwd);
+			this.conn=DriverManager.getConnection(url, user,passwd);
 		}
 
-	    
+	    public static DBConnection getInstance() throws SQLException {
+			if(instance==null) {
+				instance= new DBConnection ();}
+			else if(instance.getConn().isClosed()) {
+				instance= new DBConnection ();
+			}
+			return instance;
+		}
 	    public Connection getConn() {
 			return conn;
 		}
